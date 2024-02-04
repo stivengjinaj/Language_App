78193ce346275b44ff1b70c55a94476bfbbfc801
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-02-04 23:02:39 +0100

    Added tensorflow model to recognize letters

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,14 +9,12 @@ android {
     namespace = "com.stiven.languageapp"
     compileSdk = 34
 
-
     defaultConfig {
         applicationId = "com.stiven.languageapp"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-        //resourceConfigurations.plus(listOf("en", "it", "fr"))
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -32,6 +30,9 @@ android {
                 "proguard-rules.pro"
             )
         }
+    }
+    androidResources {
+        noCompress+="tflite"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -55,18 +56,20 @@ android {
 
 dependencies {
     val navVersion = "2.7.6"
-    val lifecycleVersion = "2.6.2"
+    val lifecycleVersion = "2.7.0"
     val roomVersion = "2.6.1"
     val coroutines = "1.7.1"
     val accompanistVersion = "0.33.2-alpha"
 
     dokkaPlugin("org.jetbrains.dokka:android-documentation-plugin:1.9.10")
+    implementation  ("io.ak1:drawbox:1.0.3")
+    implementation ("com.google.mlkit:image-labeling-custom:17.0.2")
     implementation ("androidx.appcompat:appcompat:1.7.0-alpha03")
     implementation ("com.google.accompanist:accompanist-permissions:$accompanistVersion")
-    implementation ("androidx.compose.material:material:1.5.4")
+    implementation ("androidx.compose.material:material:1.6.0")
     implementation ("com.github.ygorluizfrazao:compose-resources:1.0.0-alpha01")
     implementation ("com.airbnb.android:lottie-compose:4.0.0")
-    implementation ("androidx.compose.material:material-icons-extended:1.5.4")
+    implementation ("androidx.compose.material:material-icons-extended:1.6.0")
     implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.10")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines")
@@ -87,7 +90,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-core:1.5.4")
+    implementation("androidx.compose.material:material-icons-core:1.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
