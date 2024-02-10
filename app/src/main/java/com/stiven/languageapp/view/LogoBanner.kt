4d1d3ad53917f6b8f7144b0bf46385c799b7f8fa
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-02-10 19:42:55 +0100

    Added standard application logo view with back button.

---
+++ app/src/main/java/com/stiven/languageapp/view/LogoBanner.kt
@@ -0,0 +1,42 @@
+package com.stiven.languageapp.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * Standard application logo.
+ *
+ * @param screenSize Screen width.
+ * */
+@Composable
+fun LogoBanner(screenSize: Int){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary)
+            .height((screenSize / 6).dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "TrioLingo",
+            style = TextStyle(
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = (screenSize / 12).sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/stiven/languageapp/view/LogoBannerNavigation.kt
@@ -0,0 +1,53 @@
+package com.stiven.languageapp.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBackIosNew
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+/**
+ * Standard application logo with back icon controller.
+ *
+ * @param screenSize Screen width.
+ * @param onBackButtonClick Function to be executed when back button is clicked.
+ * */
+@Composable
+fun LogoBannerNavigation(screenSize: Int, onBackButtonClick: () -> Unit){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.primary)
+            .height((screenSize / 6).dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        IconButton(onClick = onBackButtonClick) {
+            Icon(imageVector = Icons.Rounded.ArrowBackIosNew, contentDescription = "Back")
+        }
+        Text(
+            modifier = Modifier.weight(0.3f),
+            text = "TrioLingo",
+            textAlign = TextAlign.Center,
+            style = TextStyle(
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = (screenSize / 12).sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+    }
+}
