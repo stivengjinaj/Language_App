5d54f2d8ab0a3920028960b0c4892d9024b20d49
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-02-11 21:47:46 +0100

    Added new database entity.

--- app/src/main/java/com/stiven/languageapp/AppDatabase.kt
+++ app/src/main/java/com/stiven/languageapp/AppDatabase.kt
@@ -5,20 +5,23 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.stiven.languageapp.dao.LetterDao
+import com.stiven.languageapp.dao.LettersLearntDao
 import com.stiven.languageapp.dao.StudentDao
 import com.stiven.languageapp.dao.WordDao
 import com.stiven.languageapp.model.Letter
+import com.stiven.languageapp.model.LetterLearnt
 import com.stiven.languageapp.model.Student
 import com.stiven.languageapp.model.Word
 
 /**
  * Abstract class that will be used to create the database.
  * */
-@Database(entities = [Word::class, Student::class, Letter::class], version = 2, exportSchema = false)
+@Database(entities = [Word::class, Student::class, Letter::class, LetterLearnt::class], version = 4, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
     abstract fun studentDao(): StudentDao
     abstract fun letterDao(): LetterDao
+    abstract fun lettersLearntDao(): LettersLearntDao
     companion object{
         @Volatile
         private var INSTANCE: AppDatabase? = null
