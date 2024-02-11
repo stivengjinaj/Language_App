2d53d984aae7d6c69ccc01eccc6716be00183d70
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-02-11 21:48:18 +0100

    Fixed logo banner with back button.

--- app/src/main/java/com/stiven/languageapp/view/LogoBannerNavigation.kt
+++ app/src/main/java/com/stiven/languageapp/view/LogoBannerNavigation.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -37,7 +38,11 @@ fun LogoBannerNavigation(screenSize: Int, onBackButtonClick: () -> Unit){
         horizontalArrangement = Arrangement.Start
     ) {
         IconButton(onClick = onBackButtonClick) {
-            Icon(imageVector = Icons.Rounded.ArrowBackIosNew, contentDescription = "Back")
+            Icon(
+                imageVector = Icons.Rounded.ArrowBackIosNew,
+                contentDescription = "Back",
+                tint = Color.White
+            )
         }
         Text(
             modifier = Modifier.weight(0.3f),
