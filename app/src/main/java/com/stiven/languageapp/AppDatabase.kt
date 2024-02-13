dd8985049140443352ab4d3fc7433161732d4d08
StivenGj <stiven.gjinaj@studio.unibo.it>
2024-02-14 00:00:58 +0100

    Added new pre-populated database.

--- app/src/main/java/com/stiven/languageapp/AppDatabase.kt
+++ app/src/main/java/com/stiven/languageapp/AppDatabase.kt
@@ -1,27 +1,42 @@
 package com.stiven.languageapp
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.stiven.languageapp.dao.LetterDao
 import com.stiven.languageapp.dao.LettersLearntDao
+import com.stiven.languageapp.dao.QuizDao
 import com.stiven.languageapp.dao.StudentDao
 import com.stiven.languageapp.dao.WordDao
+import com.stiven.languageapp.model.BlankQuiz
 import com.stiven.languageapp.model.Letter
 import com.stiven.languageapp.model.LetterLearnt
+import com.stiven.languageapp.model.Quiz
 import com.stiven.languageapp.model.Student
 import com.stiven.languageapp.model.Word
 
 /**
  * Abstract class that will be used to create the database.
  * */
-@Database(entities = [Word::class, Student::class, Letter::class, LetterLearnt::class], version = 5, exportSchema = false)
+@Database(
+    entities = [
+        Word::class,
+        Student::class,
+        Letter::class,
+        LetterLearnt::class,
+        BlankQuiz::class,
+        Quiz::class],
+    version = 5,
+    exportSchema = true
+)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
     abstract fun studentDao(): StudentDao
     abstract fun letterDao(): LetterDao
     abstract fun lettersLearntDao(): LettersLearntDao
+    abstract fun quizDao(): QuizDao
     companion object{
         @Volatile
         private var INSTANCE: AppDatabase? = null
@@ -36,7 +51,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "app_database"
-                ).fallbackToDestructiveMigration().build()
+                ).createFromAsset("database/app_database.db").build()
 
                 INSTANCE = instance
                 return instance
